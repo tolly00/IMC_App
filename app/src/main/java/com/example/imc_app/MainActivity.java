1b4325929e7afc6b346e0c63fc290a5d47f691ae
tolly00 <albertosylveus@gmail.com>
2021-02-23 15:08:19 -0500

    '.'

--- app/src/main/java/com/example/imc_app/MainActivity.java
+++ app/src/main/java/com/example/imc_app/MainActivity.java
@@ -3,8 +3,9 @@ package com.example.imc_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
